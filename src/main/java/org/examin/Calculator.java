abd5086f3e0de8a4f26df519f4227f6935d78da8
Manish <manish.sharmaserver008@yahoo.com>
2020-09-19 21:13:13 +0530

    Handle numbers separated by commas, to find sum.

--- src/main/java/org/examin/Calculator.java
+++ src/main/java/org/examin/Calculator.java
@@ -1,7 +1,24 @@
 package org.examin;
 
 public class Calculator {
-	public int add(String nums) {
-		return 0;
+	public int add(String text) {
+		if(text.equals("")){
+			return 0;
+		}
+		else{
+			String numList[] = splitNumbers(text, ",");
+			return sum(numList);
+		}
+	}
+	private static String[] splitNumbers(String numbers, String splitter){
+		return numbers.split(splitter);
+	}
+
+	private static int sum(String[] numbers){
+		int total = 0;
+		for(String number : numbers){
+			total += Integer.parseInt(number);
+		}
+		return total;
 	}
 }
--- src/test/java/org/examin/CalculatorTest.java
+++ src/test/java/org/examin/CalculatorTest.java
@@ -10,4 +10,19 @@ public class CalculatorTest {
 	public void testEmptyString() {
 		assertEquals(0, calculator.add(""));
 	}
+
+	@Test
+	public void testOneNumber() {
+		assertEquals(1, calculator.add("1"));
+	}
+
+	@Test
+	public void testTwoNumbers() {
+		assertEquals(3, calculator.add("1,2"));
+	}
+
+	@Test
+	public void testThreeNumbers() {
+		assertEquals(6, calculator.add("1,2,3"));
+	}
 }
